b1e02e934f5bc96f6a54db7181d55ebd71caa6f0
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2018-06-21 23:37:15 -0400

    THIS IS FXR

---
+++ README.md
@@ -0,0 +1,71 @@
+# `fxr` – CLI tool for Firefox Reality
+
+A command-line tool for installing and automating [Firefox Reality](https://github.com/MozillaReality/FirefoxReality).
+
+
+## Usage
+
+```sh
+npm install -g fxr
+fxr
+```
+
+
+## Commands
+
+To get a list of all commands and options:
+
+```sh
+fxr --help
+```
+
+#### `fxr launch <url> [options]`
+
+To launch a URL in Firefox Reality:
+
+```sh
+fxr launch http://example.com/
+```
+
+
+## CLI development
+
+To work on improving the `fxr` CLI in this repository, first ensure you've set up the project and installed the dependencies:
+
+1. Clone this git repository, and open the directory created:
+
+    ```sh
+    git clone git@github.com:MozillaReality/fxr-cli.git
+    cd fxr-cli
+    ```
+
+2. Install the [Node](https://nodejs.org/en/download/) dependencies:
+
+    ```sh
+    npm install
+    ```
+
+3. Run the CLI:
+
+    ```sh
+    node index.js
+    ```
+
+
+## License
+
+```
+Copyright 2018 Mozilla Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ commands/download.js
@@ -0,0 +1,117 @@
+const path = require('path');
+
+const fetch = require('node-fetch');
+const fs = require('fs-extra');
+const logger = require('loggy');
+
+const utils = require('../lib/utils.js');
+const SETTINGS = require('../lib/settings.js').settings;
+
+const getPlatform = utils.getPlatform;
+
+const PATHS = SETTINGS.paths;
+const PLATFORMS = SETTINGS.platforms;
+const PLATFORMS_SLUGS = SETTINGS.platformsSlugs;
+
+const URLS = {
+  taskcluster: {
+    repo: (org, repo) => `https://github.taskcluster.net/v1/repository/${org}/${repo}/master/latest`,
+    queue: (taskId) => `https://queue.taskcluster.net/v1/task/${taskId}/runs/0/artifacts`,
+    artifact: (taskId, artifactName) => `https://queue.taskcluster.net/v1/task/${taskId}/runs/0/artifacts/${artifactName}`
+  }
+};
+
+function getTaskIdFromUri (uri) {
+  return uri.split('#/')[1];
+}
+
+function parseTask (taskId, taskUrl, platformsSlugs = PLATFORMS_SLUGS) {
+  let artifacts = [];
+  function saveDownloadsIndex () {
+    const data = {
+      artifacts,
+      taskId,
+      taskUrl,
+      saved: new Date().toJSON()
+    };
+    fs.writeJsonSync(PATHS.downloads_index, data, {spaces: 2}, err => {
+      if (err) {
+        logger.error(err);
+      }
+    });
+  }
+  return fetch(URLS.taskcluster.queue(taskId))
+    .then(res => res.json())
+    .then(data => {
+      artifacts = data.artifacts.map(artifact => {
+        artifact.url = URLS.taskcluster.artifact(taskId, artifact.name);
+        artifact.basename = path.basename(artifact.name);
+        artifact.platform = artifact.storageType === 'reference' ? null : getPlatform(artifact.basename);
+        artifact.status = artifact.platform && platformsSlugs.includes(artifact.platform.slug) ? 'queued' : 'skipped';
+        return artifact;
+      });
+      saveDownloadsIndex();
+      return artifacts;
+    }).then(artifacts => {
+      return Promise.all(artifacts.map(artifact => {
+        if (artifact.status === 'skipped') {
+          artifact.status = 'skipped';
+          return Promise.resolve(artifact);
+        }
+        if (artifact.status === 'queued') {
+          artifact.status = 'downloading';
+          saveDownloadsIndex();
+        }
+        return fetch(artifact.url).then(res => {
+          return new Promise((resolve, reject) => {
+            const localFilename = path.resolve(PATHS.downloads, artifact.platform.slug, artifact.basename);
+            const destStream = fs.createWriteStream(localFilename);
+            res.body.pipe(destStream);
+            res.body.on('error', err => {
+              saveDownloadsIndex();
+              logger.error(err);
+              reject(err);
+            });
+            destStream.on('finish', () => {
+              artifact.status = 'downloaded';
+              artifact.downloaded = new Date().toJSON();
+              logger.log(`\tSuccessfully downloaded "${artifact.platform.slug}" package`);
+              saveDownloadsIndex();
+              resolve(artifact);
+            });
+            destStream.on('error', err => {
+              saveDownloadsIndex();
+              logger.error(err);
+              reject(err);
+            });
+          });
+        });
+      }));
+    }).then(() => {
+      saveDownloadsIndex();
+      return true;
+    });
+}
+
+function download(options) {
+  options = Object.assign({}, {
+    platformsSlugs: options.platformsSlugs || [SETTINGS.default_platform],
+    forceUpdate: options.forceUpdate,
+    org: options.org || SETTINGS.github_org,
+    repo: options.repo || SETTINGS.github_repo,
+  }, options);
+  fs.emptyDirSync(PATHS.downloads);
+  options.platformsSlugs.forEach(platform => fs.ensureDirSync(path.resolve(PATHS.downloads, platform)));
+  const taskRepoUrl = URLS.taskcluster.repo(options.org, options.repo);
+  return fetch(taskRepoUrl, {method: 'HEAD', redirect: 'manual'})
+    .then(res => {
+      const taskUrl = res.headers.get('location');
+      return {
+        taskId: getTaskIdFromUri(taskUrl),
+        taskUrl
+      };
+    })
+    .then(({taskId, taskUrl}) => parseTask(taskId, taskUrl, options.platformsSlugs));
+}
+
+module.exports.run = download;
